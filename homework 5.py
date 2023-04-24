ea1372dad9c7b441424e2817cd912f2de2938971
levyjeff <levyjeff@uchicago.edu>
2023-04-24 17:19:01 -0500

    Modified for split 4-5

--- homework 5.py
+++ homework 5.py
@@ -5,7 +5,7 @@
 # YOUR NAME HERE
 # YOUR GITHUB USER NAME HERE
 
-# Due date: Sunday April 23rd before midnight
+# Due date: Sunday April 30th before midnight
 # Write your answers in the space between the questions, and commit/push only
 # this file to your repo. Note that there can be a difference between giving a
 # "minimally" right answer, and a really good answer, so it can pay to put
@@ -13,42 +13,51 @@
 
 ##################
 
-# To answer these questions, you will use the two csv documents included in
-# your repo.  In nst-est2019-alldata.csv: SUMLEV is the level of aggregation,
-# where 10 is the whole US, 20 is a US region, and 40 is a US state. REGION
-# is the fips code for the US region. STATE is the fips code for the US state
-# The other values are as per the data dictionary at:
-# https://www2.census.gov/programs-surveys/popest/technical-documentation/file-layouts/2010-2019/nst-est2019-alldata.pdf
-# Note that each question will build on the modified dataframe from the
-# question before.
+# To answer these questions, you will continue from where you left off in
+# homework 4.  Your dataframe should have 5 columns: STATE, POPESTIMATE
+# for 2020, 2021, and 2022, and POPCHANGE.  Each row should be a US state.
 
-# Question 1: Load the population estimates file into a dataframe. Specify
-# an absolute path using the Python os library to join filenames, so that
-# anyone who clones your homework repo only needs to update one for all
-# loading to work.  Then show code doing some basic exploration of the
-# dataframe; imagine you are an intern and are handed a dataset that your
-# boss isn't familiar with, and asks you to summarize for them.
+# Question 1a: Reshape the data from wide to long, using the wide_to_long function,
+# making sure you reset the index to the default values if any of your data is located 
+# in the index.  What happened to the POPCHANGE column, and why should it be dropped?
 
+#Question 1b: Repeat the reshaping using the melt method.  Clean up the result so
+#that it is the same as the result from 4a (without the POPCHANGE column).
 
-# Question 2: Your data only includes fips codes for states.  Use the us
-# library to crosswalk fips codes to state abbreviations.  Keep only the
-# state abbreviations in your data.
+# Question 2: Your Open the state-visits.xlsx file, and fill in the VISITED column
+# with a dummy variable for whether you've visited a state or not.  If you
+# haven't been to many states, then filling in a random selection of them
+# is fine too.  Save your changes.  Then load the xlsx file as a dataframe in
+# Python, and merge the visited column into your original long-form population 
+# dataframe, only keeping values that appear in both dataframes.  Are any 
+# observations dropped from this?  Show code where you investigate your merge, 
+# and display any observations that weren't in both dataframes.
 
+# Question 3a: The file policy_uncertainty.xlsx contains monthly measures of 
+# economic policy uncertainty for each state.  The EPU_National column esimates
+# uncertainty from national sources, EPU_State from state, and EPU_Composite 
+# from both (EPU-N, EPU-S, EPU-C).  Load it as a dataframe, then calculate 
+# the mean EPU-C value for each state/year, leaving only columns for state, 
+# year, and EPU_Composite, with each row being a unique state-year combination.
 
-# Question 3: Subset the data so that only observations for individual
-# US states remain, and only state names and data for the population
-# estimates in 2010-2019 remain.
+# Question 3b) Reshape the EPU data into wide format so that each row is unique 
+# by state, and the columns represent the EPU-C values for the years years 2022, 
+# 2021, and 2020. 
 
+# Question 3c) Finally, merge this data into your merged data from question 2, 
+# making sure the merge does what you expect.
 
-# Question 4: Reshape the data from wide to long, making sure you reset
-# the index to the default values if any of your data is located in the index.
+# Question 4: Using groupby on the VISITED column in the dataframe resulting
+# from the previous question, answer the following questions and show how you 
+# calculated them: a) what is the single smallest state by 2022 population 
+# that you have visited, and not visited?  b) what are the three largest states 
+# you have visited, and the three you have not visited? c) do states you have 
+# visited or states you have not visited have a higher average EPU-C value in 2022?
 
-
-# Question 5: Open the state-visits.csv file with Excel, and fill in the 
-# VISITED column with a dummy variable for whether you've visited a state or 
-# not.  If you haven't been to many states, then filling in a random selection 
-# of them is fine too.  Save your changes.  Then load the file as a dataframe 
-# in Python, and merge the visited column into your population dataframe, only
-# keeping values that appear in both dataframes.  Are any observations
-# dropped from this?  Show code where you investigate your merge, and
-# display any observations that weren't in both dataframes.
+# Question 5: Transforming data to have mean zero and unit standard deviation
+# is often called "standardization", or a "zscore".  The basic formula to 
+# apply to any given value is: (value - mean) / std
+# Return to the long-form EPU data you created in step 3a and then, using groupby
+# and a function you write, transform the data so that the values for EPU-C
+# have mean zero and unit standard deviation for each state.  Add these values
+# to a new column named EPU_C_zscore.
